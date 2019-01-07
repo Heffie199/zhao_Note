e7aea5e41acce609430ee78167df8951e85c6c4b
zhaodahan <1791154630@qq.com>
2019-01-07 15:09:00 +0800

    'pic_synchronized'

---
+++ 一键提交.bat
@@ -0,0 +1,19 @@
+
+cd E:\mygitRes\zhao_Note\zhao_Note
+
+echo .....!!!!git checkout note master 
+git pull origin master
+git  add .
+git commit  -m 'pic_synchronized'
+git push origin master
+echo .....enter any key note to exit
+
+cd E:\mygitRes\zhao_Note\zhao_Note.wiki\zhao_Note.wiki
+echo .....!!!!git checkout wiki master 
+git pull origin master
+git  add .
+git commit  -m 'wiki_synchronization'
+git push origin master
+echo .....enter any key to exit
+
+pause>nul
