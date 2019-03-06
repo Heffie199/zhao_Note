aa1649efea2e18f63a03934d8a0edbb0968b3b46
zhaodahan <1791154630@qq.com>
2019-03-06 11:24:20 +0800

    'pic_synchronized'

--- 一键提交.bat
+++ 一键提交.bat
@@ -16,4 +16,13 @@ git commit  -m 'wiki_synchronization'
 git push origin master
 echo .....enter any key to exit
 
+
+cd E:\mygitRes\zhao_project
+echo .....!!!!git checkout project master 
+git pull origin master
+git  add .
+git commit  -m 'project_synchronization'
+git push origin master
+echo .....enter any key to exit
+
 pause>nul
